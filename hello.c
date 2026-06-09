6369fd132f14e1342150cf9014f15a7774ab75ea
ManuPapst <emanuela.papst-luiz@rdgstudent.com>
2026-06-09 15:56:37 +0000

    first commit

---
+++ hello.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(void) {
+    printf("Hello, world!\n");
+    printf("This is a simple C program.\n");
+
+    int *numbers = malloc(20 * sizeof(int));
+    if (numbers == NULL) {
+        fprintf(stderr, "Failed to allocate memory\n");
+        return 1;
+    }
+
+    for (int i = 0; i < 20; ++i) {
+        numbers[i] = i + 1;
+    }
+
+    printf("Allocated space for 20 integers.\n");
+    for (int i = 0; i < 20; ++i) {
+        printf("numbers[%d] = %d\n", i, numbers[i]);
+    }
+
+    free(numbers);
+    return 0;
+}
