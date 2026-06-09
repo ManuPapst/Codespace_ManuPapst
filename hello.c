9d85d747b40f85d5d274619f6a92326a1c5dba30
ManuPapst <emanuela.papst-luiz@rdgstudent.com>
2026-06-09 17:00:37 +0000

    added space for organization.

--- hello.c
+++ hello.c
@@ -13,9 +13,9 @@ int main(void) {
 
     for (int i = 0; i < 20; ++i) {
         numbers[i] = i + 1;
-    }
+    } 
     printf("Numbers from 1 to 20:\n");
-    
+
     printf("Allocated space for 20 integers.\n");
     for (int i = 0; i < 20; ++i) {
         printf("numbers[%d] = %d\n", i, numbers[i]);
